538793973818d13675ef751c6a0068e123fc31c2
sk8forether <5706491+sk8forether@users.noreply.github.com>
2018-05-14 22:45:04 +0000

    setup to test on spitler FRB data

--- spitler_pipeline/params.py
+++ spitler_pipeline/params.py
@@ -1,21 +1,23 @@
 # Much of this code has been copied and modified from the rsw_pipeline written by Robert Wharton
 
 # FITS Directory
-fits_dir = "/mnt/data1/rsw_test/pipeline/psrfits"
+#fits_dir = "/mnt/data1/rsw_test/pipeline/psrfits"
+fits_dir = "/mnt/data1/gold_copies/spitler_FRB"
 
 # Base name of fits files in FITS dir
-basename = "waller"
+#basename = "waller"
+basename = "waller_test_combined_0001"
 
 # Search output directory
-search_dir = "/mnt/data1/rsw_test/pipeline/search"
+search_dir = "/mnt/data1/pete/test/spitler_pipeline/search"
 
 # Processing steps to do
-do_rfifind    = 1      # Run PRESTO rfifind and generate a mask
-do_prepsub    = 1      # Run PRESTO prepsubband dedispersion
-do_fft        = 0      # FFT *dat files before accelsearch
-do_candsearch = 0      # Run PRESTO accelsearch on the data
-do_presto_sp  = 1      # Run PRESTO singlepulse.py
-do_param_cp   = 0      # copy parameter file to output directory 
+do_rfifind    = 1       # Run PRESTO rfifind and generate a mask
+do_prepsub    = 1       # Run PRESTO prepsubband dedispersion
+do_fft        = 0       # FFT *dat files before accelsearch
+do_candsearch = 0       # Run PRESTO accelsearch on the data
+do_presto_sp  = 1       # Run PRESTO singlepulse.py
+do_param_cp   = 0       # copy parameter file to output directory 
 
 # data format type
 # Use PRESTO naming conventions (ie, SIGPROC filterbank => 'filterbank'
@@ -24,15 +26,15 @@ dat_type = 'psrfits'
 
 # Locations of all the required scripts
 #singlepulse = 'single_pulse_search.py'
-singlepulse = "/mnt/data1/rsw_test/single_pulse_search.py"
+singlepulse = "/mnt/data1/pete/code/FRB_pipeline/spitler_pipeline/single_pulse_search.py"
 
 # rfifind params
-use_mask  = 1     # use the rfi mask?
-rfi_time  = 5.0   # sec, time over which stats are calc'd in rfifind (d: 30)
-time_sig  = 15    # The +/-sigma cutoff to reject time-domain chunks (d: 10)
-freq_sig  = 6     # The +/-sigma cutoff to reject freq-domain chunks (d: 4)
-chan_frac = 0.3   # The fraction of bad channels that will mask a full interval (d: 0.7)
-int_frac  = 0.3   # The fraction of bad intervals that will mask a full channel (d: 0.3)
+use_mask  = 1           # use the rfi mask?
+rfi_time  = 5.0         # sec, time over which stats are calc'd in rfifind (d: 30)
+time_sig  = 15          # The +/-sigma cutoff to reject time-domain chunks (d: 10)
+freq_sig  = 6           # The +/-sigma cutoff to reject freq-domain chunks (d: 4)
+chan_frac = 0.3         # The fraction of bad channels that will mask a full interval (d: 0.7)
+int_frac  = 0.3         # The fraction of bad intervals that will mask a full channel (d: 0.3)
 rfi_otherflags = '-noscales -nooffsets -noweights ' #
 
 # Dedispersion parameters
@@ -41,31 +43,31 @@ rfi_otherflags = '-noscales -nooffsets -noweights ' #
 dmlow = 500 
 ddm   = 5
 dmcalls    = 1
-dmspercall = 20    # 250
-nsub       = 960    # 608
+dmspercall = 20         # 250
+nsub       = 960        # 608
 dsubDM     = 0.0
 downsample = 1
 prep_otherflags = '-noscales -nooffsets -noweights ' # other flags for prepsubband
 
 # accelsearch parameters
-use_fft = 1        # Search FFTs (1) instead of dats (0)
-zmax = 100         # Max acceleration in fourier bins
-numharm = 16       # Number of harmonics to sum
-freq_lo = 0.1      # Hz, lowest freq to consider real
-freq_hi = 2000.0   # Hz, highest freq to consider real
-zap_str = '' # Put zap stuff here, if desired
-accel_cores = 1   # number of processing cores
+use_fft = 1             # Search FFTs (1) instead of dats (0)
+zmax = 100              # Max acceleration in fourier bins
+numharm = 16            # Number of harmonics to sum
+freq_lo = 0.1           # Hz, lowest freq to consider real
+freq_hi = 2000.0        # Hz, highest freq to consider real
+zap_str = ''            # Put zap stuff here, if desired
+accel_cores = 1         # number of processing cores
 
 # Single pulse
-max_width = 1.0   # Max pulse width (seconds)
-dtrend    = 1     # Detrend factor 1-32 powers of two 
-sp_otherflags = "-f " # Other flags
+max_width = 1.0         # Max pulse width (seconds)
+dtrend    = 1           # Detrend factor 1-32 powers of two 
+sp_otherflags = "-f "   # Other flags
 
 
 # Sifting parameters (copied mostly from PRESTO's ACCEL_sift.py):
 #--------------------------------------------------------------
-min_num_DMs = 2      # Min number of DM bins a candidate must show up in to be "good"
-low_DM_cutoff = 0.0  # Lowest DM to be considered a "real" pulsar
+min_num_DMs = 2         # Min number of DM bins a candidate must show up in to be "good"
+low_DM_cutoff = 0.0     # Lowest DM to be considered a "real" pulsar
 
 # The following show up in the sifting.py PRESTO module:
 
