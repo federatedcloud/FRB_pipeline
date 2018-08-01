20ec9b9f4090de7c1293ec307d961123e1007bf1
Peter Vaillancourt <pzv2@cornell.edu>
2018-08-01 11:56:49 -0400

    Prototype (#32)
    * Adding basic configparser
    
    * Added combine_mocks method with subprocess calls
    
    * Updated FOF algorithm. Added cluster grouping.
    
    * Deleted double code
    
    * Added numpy array method
    
    * Renamed Config folder to Pipeline
    
    * Added FOF method to pipeline
    
    * Fixed friends.py; now uses global variable implementation. FOF_method.py arranges the parameters to be used as global variables into a new dictionary called gd, which is passed to the fof() method. Messed around with parameter values in simpleFOF.cfg.
    
    * Fixes #18
    
    * Added remove_comments to readconfig and other minor changes

---
+++ Pipeline/Templates/README.md
@@ -0,0 +1,5 @@
+# Templates
+
+These template configuration files are intended to give examples for running the FRB pipeline with various methods and parameter settings.
+
+
---
+++ Pipeline/readconfig.py
@@ -0,0 +1,50 @@
+import sys
+import configparser
+from collections import OrderedDict
+
+config = configparser.SafeConfigParser(dict_type=OrderedDict, allow_no_value=True)
+
+def print_config(config):
+    for x in config.sections():
+        print("[%s]"%(x))
+        for(x, value) in config.items(x):
+            print("%s=%s"%(x,value))
+        print()
+
+def read_config(filename, dictionary={}):
+    config.read(filename)
+    
+    # Check if data is the first section
+    if not ('data' in config.sections()):
+        sys.exit("Error: the selected configuration file does not contain the "
+            "[data] section, which is necessary.  Quitting.\n")
+    
+    # Put information in the dirctionary
+    for x in config.sections():
+        for(x, value) in config.items(x):
+            dictionary[x] = remove_comments(value)
+    
+    return dictionary
+
+def remove_comments(value):
+    return value.split(";")[0]
+
+
+####################
+##     MAIN       ##
+####################
+
+if __name__ == "__main__":
+    websters = {}
+    read_config("Templates/simpleFOF.cfg", websters)
+    #read_config("Templates/default.cfg", websters)
+    
+    # Debugging
+    #print_config(config)
+    #print("============")
+    #print(websters)
+    
+    
+    #methods = config.sections()
+
+
