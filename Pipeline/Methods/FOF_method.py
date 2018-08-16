2e9b879ec59a34096a9c21cf1322b7116038a8d8
sk8forether <pzv2@cornell.edu>
2018-08-16 20:22:12 +0000

    Moved FOF to Modules

--- Pipeline/Methods/FOF_method.py
+++ Pipeline/Methods/FOF_method.py
@@ -1,41 +1,37 @@
-from method import *
-
 import sys
-sys.path.insert(0, '../FOF')
+sys.path.insert(0, '../Modules')
+from method import *
 from friends import *
 
 
 def main(d):
     print("Running Friend-Of-Friends")
     
-    # Fix strings
-    # TODO: do this better later
+    # TODO: do conversion in readconfig?
     
     # Set up fof inputs
     np_data = d['np_data'] 
-    m1 = float(fix_str(d['m1']))
-    m2 = float(fix_str(d['m2']))
-    t_gap = int(fix_str(d['t_gap']))
-    v_gap = int(fix_str(d['v_gap']))
-    tstart = float(fix_str(d['tstart']))
+    m1 = float(d['m1'])
+    m2 = float(d['m2'])
+    t_gap = int(d['t_gap'])
+    v_gap = int(d['v_gap'])
+    tstart = float(d['tstart'])
     
+    # global dictionary
     gd = {}
-    gd['tsamp'] = int(fix_str(d['tsamp']))
-    gd['vsamp'] = int(fix_str(d['vsamp']))
-    #dt = float(fix_str(d['TBIN']))
+    gd['tsamp'] = int(d['tsamp'])
+    gd['vsamp'] = int(d['vsamp'])
+    #dt = float(d['TBIN'])
     gd['dt'] = float(d['TBIN'])
-    #dv = float(fix_str(d['CHAN_BW']))
+    #dv = float(d['CHAN_BW'])
     dv = abs(float(d['CHAN_BW'])) # for some reason this was negative.
     gd['dv'] = dv
-    #vlow = float(fix_str(d['OBSFREQ'])) - dv * float(fix_str(d['NCHAN'])) / 2.0
-    #vhigh = float(fix_str(d['OBSFREQ'])) + dv * float(fix_str(d['NCHAN'])) / 2.0
+    #vlow = float(d['OBSFREQ']) - dv * float(d['NCHAN']) / 2.0
+    #vhigh = float(d['OBSFREQ']) + dv * float(d['NCHAN']) / 2.0
     gd['vlow'] = float(d['OBSFREQ']) - dv * float(d['NCHAN']) / 2.0
     gd['vhigh'] = float(d['OBSFREQ']) + dv * float(d['NCHAN']) / 2.0
-    # run algorithm
+    
+    # Run algorithm
     fof(gd, np_data, m1, m2, t_gap, v_gap, tstart)
     
     return d
-
-
-def fix_str(input):
-    return input.split(";")[0]
