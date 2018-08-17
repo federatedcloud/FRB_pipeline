b6959ae2a63be008e7ddb7baf7b8ab61641617fd
sk8forether <pzv2@cornell.edu>
2018-08-17 20:08:02 +0000

    Dynamically importing methods

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -1,9 +1,12 @@
+# Execution of pipeline happens here
+
 import sys
 sys.path.insert(0, './Methods')
 sys.path.insert(0, './Modules')
 
 #import os, time
 #import subprocess as sp
+import importlib
 import readconfig as cfg
 from optparse import OptionParser
 
@@ -22,8 +25,9 @@ hotpotato = {}
 
 # Step 1 - Read the config file
 hotpotato = cfg.read_config("Templates/simpleFOF.cfg")
+#hotpotato = cfg.read_config()
+
 # TODO: filename should be passed in as an argument and should otherwise run a default.cfg
-# default.cfg needs to be set up.  Maybe have it list all possible methods/options?
 
 
 # Step 2 - TODO: Organize parameters for each method into a dictionary
@@ -31,32 +35,37 @@ hotpotato = cfg.read_config("Templates/simpleFOF.cfg")
 # but it should be done here so all conversion happens here
 
 
-# Step 3 - TODO: import method files based on their existence as a section in cfg file
-
-
-# Step 4 - If combine_mocks method is defined, call it first
-if hotpotato['combine']:
-    import combine_mocks_method as combine
+# Step 3 - If combine_mocks method is defined, call it first
+if 'combine_mocks' in hotpotato['methods']:
+    # TODO: error check this
+        #if ((not cfg.config.has_option('file1')) or (not cfg.config.has_option('file2'))):
+        #    sys.exit("\n Error: Configuration file is not set up correctly:"
+        #               " combine = True but file1 and file2 are not properly defined") 
+        #elif ((hotpotato['file1'] == '') and (hotpotato['file2'] == '')):
+        #    sys.exit("\n Error: Configuration file is not set up correctly:"
+        #               " combine = True but file1 and file2 are not properly defined")
+    
+    combine = __import__('combine_mocks' + '_method')
     combine.main(hotpotato)
 
 
-# Step 5 - create a dynamic spectra as numpy array
-import fits2npz_method as f2n
-hotpotato = f2n.main(hotpotato)
+# Step 4 - create a dynamic spectra as numpy array
+if hotpotato['use_np_array']:
+    import fits2npz_method as f2n
+    hotpotato = f2n.main(hotpotato)
 
 
-# Step 6 - Call FOF method
-import FOF_method as FOF
-hotpotato = FOF.main(hotpotato)
+# Step 5 - dynamically import and call the main function of each method defined in cfg
+for x in hotpotato['methods']:
+    if (x == 'data' or x == 'combine_mocks'):
+        continue
+    temp = __import__(x + '_method')
+    temp.main(hotpotato)
 
 
-# Step 7 - Exit cleanly
+# Step 6 - Exit cleanly
 # TODO: remove combined file?
 sys.exit("\n Pipeline tasks completed \n=====")
-
-
-
-
 
 
 # Optional Steps
@@ -64,10 +73,4 @@ sys.exit("\n Pipeline tasks completed \n=====")
 #  Check dependencies
 #  Basic error checking of config
 #  Error checking of methods
-
-
-
-
-
-
 
