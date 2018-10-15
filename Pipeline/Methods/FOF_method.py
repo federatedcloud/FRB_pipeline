c599c559779f8d596930e2c212a7e924a7dd404a
Plato Deliyannis <pad233@cornell.edu>
2018-10-15 03:38:06 +0000

    small changes

--- Pipeline/Methods/FOF_method.py
+++ Pipeline/Methods/FOF_method.py
@@ -7,7 +7,7 @@ from friends import *
 def main(d):
     print("Running Friend-Of-Friends")
     
-    # Set up fof inputs
+    # Set up fof-specific parameters
     np_data = d['np_data']
     m1 = d['m1']
     m2 = d['m2']
@@ -15,12 +15,13 @@ def main(d):
     v_gap = int(d['v_gap'])
     tstart = d['tstart']
     
-    # global dictionary
-    gd = {}
+    # set up dictionary of global parameters
+    gd = {} 
     gd['tsamp'] = int(d['tsamp'])
     gd['vsamp'] = int(d['vsamp'])
     gd['dt'] = d['TBIN']
     gd['dv'] = abs(d['CHAN_BW']) # for some reason this was negative
+    dv = gd['dv']
     gd['vlow'] = d['OBSFREQ'] - dv * d['NCHAN'] / 2.0
     gd['vhigh'] = d['OBSFREQ'] + dv * d['NCHAN'] / 2.0
     
