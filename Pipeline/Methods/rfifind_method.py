e6c2fdbafaf7bd7f7036f66e6e1db05551498977
sk8forether <pzv2@cornell.edu>
2019-03-15 18:10:14 +0000

    Spacing

--- Pipeline/Methods/rfifind_method.py
+++ Pipeline/Methods/rfifind_method.py
@@ -8,9 +8,9 @@ from glob import glob
 
 def main(d):
 
-    print("Running PRESTO rfifind.")
+    print("Running PRESTO rfifind")
     t_rfi_start = time.time()
-   
+    
     # get/set file locations 
     fits_dir = d['directory']
     rfi_dir = d['work_dir'] + d['rfi_dir_name']
@@ -18,7 +18,7 @@ def main(d):
     fitslist = glob('%s/%s*.fits' %(fits_dir, fitsname))
     fitslist.sort()
     fitsfiles = ' '.join(fitslist)
-
+    
     # get parameters from dictionary
     rfi_time = int(float(d['rfi_time']))
     tsig = float(d['tsig'])
@@ -26,7 +26,7 @@ def main(d):
     chanfrac = float(d['chanfrac'])
     intfrac = float(d['intfrac'])
     rfi_otherflags = d['rfi_otherflags'] + ' '
-
+    
     # run command    
     cmd = 'rfifind -psrfits -o %s -time %d -timesig %f -freqsig %f '\
           '-chanfrac %f -intfrac %f %s %s' %(fitsname, rfi_time, tsig, fsig,
@@ -40,9 +40,9 @@ def main(d):
 
     cmd = 'mv ./*rfifind.* ' + rfi_dir
     try_cmd(cmd)
-
+    
     t_rfi_end = time.time()
     rfi_time = (t_rfi_end - t_rfi_start)
     print("RFI Flagging took %f seconds" %(rfi_time))
-
+    
     return d
