e271f8aab6ba4fcab3ec5442c3f31dec0fba0d29
sk8forether <pzv2@cornell.edu>
2019-03-26 20:32:03 +0000

    Improved to include options for new methods and output

--- Pipeline/Methods/results_method.py
+++ Pipeline/Methods/results_method.py
@@ -2,23 +2,38 @@
 from method import *
 
 
-def main(dictionary):
+def main(d):
     print("Organizing output files")
     
-    results_dir = dictionary['output_directory']
+    results_dir = d['output_directory']
     
     # Move generated .npz file
-    if (dictionary['move_npz_file'] == True):
-        cmd = "mv %s.npz %s" %(dictionary['filename_npz'], results_dir)
+    if (d['move_npz_file'] == True):
+        cmd = "mv %s.npz %s" %(d['filename_npz'], results_dir)
         try_cmd(cmd)
     
     # Move generated combined .fits file
-    if (dictionary['move_combined_file'] == True):
-        cmd = "mv %s.fits %s" %(dictionary['fitsname'], results_dir)
+    if (d['move_combined_file'] == True):
+        cmd = "mv %s.fits %s" %(d['fitsname'], results_dir)
         try_cmd(cmd)
-        
+    
+    # Move filterbank file
+    if (d['move_maskdata_file'] == True):
+        cmd = "mv raw_data_with_mask.fits %s/masked_dynamic_spectra.fil" %(results_dir)
+        try_cmd(cmd)
+    
+    # Move generated text files
+    if (d['move_txt_files'] == True):
+        cmd = "mv *.txt %s" %(results_dir)
+        try_cmd(cmd)
+    
+    # Move generated png files
+    if (d['move_png_files'] == True):
+        cmd = "mv *.png %s" %(results_dir)
+        try_cmd(cmd)
+    
     
     # Insert more move commands in here as needed to organize results
     
-    return dictionary
+    return d
 
