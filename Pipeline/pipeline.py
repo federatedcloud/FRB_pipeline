d1393b3151864e545721fa5e59a01827cfcf11da
sk8forether <pzv2@cornell.edu>
2019-07-01 15:59:26 +0000

    Moved conversion to readconfig and fixed removal of spaces

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -32,7 +32,7 @@ def main():
     hotpotato = cfg.read_config(args.configpath)
     
     # Ensure proper conversion of config file and header parameters in dictionary
-    hotpotato = convert_values(hotpotato)
+    hotpotato = cfg.convert_values(hotpotato)
     
     # Dynamically import and call the main function of each method defined in cfg
     for x in hotpotato['methods']:
@@ -52,52 +52,6 @@ def main():
     #  Error checking of methods
     #===============================
 
-
-# convert dictionary string values to float or int if they are numbers
-def convert_values(d):
-    for x in d:
-        if isinstance(d[x], str):
-            if is_bool(d[x]):
-                d[x] = to_bool(d[x])
-            elif is_float(d[x]):
-                d[x] = float(d[x])
-            elif is_int(d[x]):
-                d[x] = int(d[x])
-            else:
-                continue
-    
-    return d
-
-# check if input can be converted to bool, return bool (of if it can)
-def is_bool(input):
-    if ((input in true_values) or (input in false_values)):
-        return True
-    else:
-        return False
-
-def to_bool(input):
-    if input in true_values:
-        return True
-    else:
-        return False
-
-# check if input can be converted to float, return bool
-def is_float(input):
-    try:
-        n = float(input)
-    except ValueError:
-        return False
-    
-    return True
-
-# check if input can be converted to int, return bool
-def is_int(input):
-    try:
-        n = int(input)
-    except ValueError:
-        return False
-    
-    return True
 
 
 ####################
--- Pipeline/readconfig.py
+++ Pipeline/readconfig.py
@@ -25,10 +25,7 @@ def read_config(filename, dictionary={}):
     # Put information in the dirctionary
     for x in config.sections():
         for(x, value) in config.items(x):
-            if (is_bool(value)):
-                dictionary[x] = to_bool(remove_spaces(value))
-            else:
-                dictionary[x] = remove_comments(value)
+            dictionary[x] = remove_spaces(remove_comments(value))
     
     dictionary['methods'] = config.sections()
     
@@ -38,11 +35,27 @@ def remove_comments(value):
     return value.split(";")[0]
 
 def remove_spaces(value):
-    return value.split()[0]
+    temp = value.strip()
+    return temp.split()[0]
+
+# convert dictionary string values to float or int if they are numbers
+def convert_values(d):
+    for x in d:
+        if isinstance(d[x], str):
+            if is_bool(d[x]):
+                d[x] = to_bool(d[x])
+            elif is_int(d[x]):
+                d[x] = int(d[x])
+            elif is_float(d[x]):
+                d[x] = float(d[x])
+            else:
+                continue
+    
+    return d
 
 # check if input can be converted to bool, return bool (of if it can)
 def is_bool(input):
-    if ((remove_spaces(input) in true_values) or (remove_spaces(input) in false_values)):
+    if ((input in true_values) or (input in false_values)):
         return True
     else:
         return False
@@ -53,6 +66,24 @@ def to_bool(input):
     else:
         return False
 
+# check if input can be converted to float, return bool
+def is_float(input):
+    try:
+        n = float(input)
+    except ValueError:
+        return False
+    
+    return True
+
+# check if input can be converted to int, return bool
+def is_int(input):
+    try:
+        n = int(input)
+    except ValueError:
+        return False
+    
+    return True
+
 
 
 ####################
@@ -61,7 +92,9 @@ def to_bool(input):
 
 if __name__ == "__main__":
     websters = {}
-    websters = read_config("Templates/default.cfg")
+    websters = read_config("Templates/test.cfg")
+    
+    websters = convert_values(websters)
     
     # Debugging
     print_config(config)
