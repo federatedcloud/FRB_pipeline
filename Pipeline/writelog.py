c1d620c3ee781cbcc19b8e20f1e9b39b31089068
sk8forether <pzv2@cornell.edu>
2020-02-14 14:55:18 +0000

    Move logs to Logs directory

--- Pipeline/writelog.py
+++ Pipeline/writelog.py
@@ -4,11 +4,10 @@ import datetime
 logger = logging.getLogger(__name__)
 
 def start_log():
-    logfile = "run_" + datetime.datetime.now().strftime("%B_%d_%Y_%H_%M_%S") + ".log"
+    logfile = "Logs/run_" + datetime.datetime.now().strftime("%B_%d_%Y_%H_%M_%S") + ".log"
     logging.basicConfig(filename=logfile, level=logging.DEBUG)
 
 def log_it(message=""):
     logger.debug("\n" + message)
     print(message)
-
 
